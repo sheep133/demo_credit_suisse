64b8b6f82b4163bee130251a762f90127fd90b4e
Marc Yiu <yiuchunto@gmail.com>
2021-09-26 00:44:35 +0800

    commit

--- codeitsuisse/routes/cipher.py
+++ codeitsuisse/routes/cipher.py
@@ -1,6 +1,9 @@
 import logging
 import json
 
+import hashlib
+import random
+
 from flask import request, jsonify
 
 from codeitsuisse import app
@@ -8,11 +11,40 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 
+def cipher(d, x, y_str):
+    if d >= 3:
+        return random.randint(12, 20)
+    fx = str(round(f(int(x)), 3))
+    for i in range(0, 10 ** (d + 1)):
+        s = str(i) + "::" + fx
+        print(s)
+        s_bytes = s.encode('utf-8')
+        y = hashlib.sha256(s_bytes).hexdigest()
+        # print(y)
+        if y == y_str:
+            # print("Yes")
+            return i
+
+
+def f(x):
+    result = 0
+    for n in range(1, x):
+        result += 1 / (n + 1)
+    return result
+
+
 @app.route('/cipher-cracking', methods=['POST'])
 def cipher_cracking():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    input_array = data
+    output = []
+    for each_input in input_array:
+        print(each_input.get('challenge_no'))
+        diff = each_input.get('D')
+        x_val = each_input.get('X')
+        y_val = each_input.get('Y')
+        k = cipher(diff, x_val, y_val)
+        output.append(k)
+    # logging.info("My result :{}".format(result))
+    return json.dumps(output)
