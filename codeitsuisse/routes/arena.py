2ed8835066f117f40dc4d5387ec4881f32659487
Marc Yiu <yiuchunto@gmail.com>
2021-09-25 17:19:27 +0800

    commit

--- codeitsuisse/routes/arena.py
+++ codeitsuisse/routes/arena.py
@@ -1,6 +1,7 @@
 import logging
 import json
 
+import requests
 from flask import request, jsonify
 
 from codeitsuisse import app
@@ -8,12 +9,14 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 
-@app.route('/tic-tac-toe', methods=['POST'])
+@app.route('/tic-tac-toe', methods=['GET', 'POST'])
 def tic_tac_toe():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
+    battle_id = data.get("battleId")
+
+    battle_event = requests.get('https://cis2021-arena.herokuapp.com/tic-tac-toe/start/%s' % battle_id)
 
     # result = inputValue * inputValue
     # logging.info("My result :{}".format(result))
-    # return json.dumps(result)
+    return json.dumps(data)
